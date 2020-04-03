50048ec09b0830f0ed44fa1f54ed0112024143b4
culshaw <culshaw@gmail.com>
2020-04-03 09:58:51 +0100

    Update and rename getPackageVersion.ts to getPackageNodeVersion.ts

--- src/getPackageNodeVersion.ts
+++ src/getPackageNodeVersion.ts
@@ -16,5 +16,5 @@ export const findPackageJson = (path: string): string => {
 export const getPackageVersion = (path: string): string => {
   const packageJson = findPackageJson(path);
 
-  return JSON.parse(packageJson).version;
+  return JSON.parse(packageJson).engines.node;
 };
